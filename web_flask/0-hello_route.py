26330f5cf470cdecd315cd42146aa37db07535ac
Mohamed Mouyahada <mouyahada@gmail.com>
2024-07-29 01:39:34 +0100

    web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+"""Script to start a Flask Web Application"""
+from flask import Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_world():
+    """Hello HBNB"""
+    return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0', port=5000)
